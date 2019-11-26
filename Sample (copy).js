c3c1f01edde6b7dbb08d2a504fe46ed76198f2d1
goes98 <goes98@gmail.com>
2019-11-26 18:12:46 +0530

    SSM

---
+++ Sample (copy).js
@@ -0,0 +1,3 @@
+console.log("Sunbeam here...!!!...1");
+console.log("Sunbeam here...!!!...2");
+
--- Sample.js
+++ Sample.js
@@ -3,3 +3,6 @@ console.log("Sunbeam here...!!!...2");
 console.log("Sunbeam here...!!!...3");
 console.log("Sunbeam here...!!!...4");
 console.log("Sunbeam here...!!!...5");
+
+console.log("Sunbeam here...!!!...6");
+console.log("Sunbeam here...!!!...7");
